99e995b59fd311f0b20e3cf398f75376eca8ff43
nawaf alowain <nawaf.alowain@gmail.com>
2021-10-09 01:19:15 +0300

    Add files via upload

---
+++ pwdReset/config.php
@@ -0,0 +1,7 @@
+<?php
+$servername = "localhost";
+$username = "hawy_root1";
+$password = "hellodarknessmyoldfriend";
+$database = "hawy_AssetManagementSystem";
+$conn = mysqli_connect($servername,$username,$password,$database);
+?>
---
+++ pwdReset/loginPart2.php
@@ -0,0 +1,6 @@
+//this code should be in login page
+<html>
+<form>
+    <a href="reset-password.php">Forgot your password</a>
+    </form>
+</html>
